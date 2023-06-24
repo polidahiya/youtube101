239da80fa5fbb9e3324df243064a8464bf78ec80
Parvesh <121171390+polidahiya@users.noreply.github.com>
2023-06-24 11:00:47 +0530

    Update home.js

--- routers/home.js
+++ routers/home.js
@@ -7,38 +7,35 @@ const home = express.Router();
 const app = express();
 app.use(cookieParser());
 
-//
-const db_link = "mongodb+srv://polidahiya830:12er56ui90%40Poli@cluster0.pvrgiqn.mongodb.net/?retryWrites=true&w=majority";
-
-//
+const db_link =
+  "mongodb+srv://polidahiya830:12er56ui90%40Poli@cluster0.pvrgiqn.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose
   .connect(db_link)
   .then(async function () {
-    const client =await new MongoClient(db_link);
-    const db =await client.db("youtube");
-    // const users = db.collection("users");
-    const videos =await db.collection("videos");
+    const client = new MongoClient(db_link);
+    await client.connect();
+    const db = client.db("youtube");
+    const videos = db.collection("videos");
     console.log("db connected home");
+
     home.get("/home/:videotype", async (req, res) => {
       try {
         const { videotype } = req.params;
         const video = videotype.replace(/:/g, "");
 
         const data = await videos.find({ videotype: video }).toArray();
-        // const data = await videos.find({videotype:video}).exec();   mongo@6
-        // const data = await videos.find({ postby: "email" }).toArray();
         console.log("Retrieved data:", data);
         res.json(data);
         console.log("home req");
       } catch (error) {
-        console.log("gethome error:" + error);
+        console.log("gethome error: " + error);
+        res.status(500).json({ error: "Internal server error" });
       }
     });
   })
   .catch(function (err) {
     console.log(err);
   });
-//
 
 module.exports = home;
