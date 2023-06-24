b61295d48d74255f8db396b839c36b6e8bdaefe9
Parvesh <121171390+polidahiya@users.noreply.github.com>
2023-06-24 18:45:22 +0530

    Update login.js

--- routers/login.js
+++ routers/login.js
@@ -6,51 +6,70 @@ const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 
 const login = express.Router();
-login.route("/login").post(postlogin);
 
 const db_link =
   "mongodb+srv://polidahiya830:12er56ui90%40Poli@cluster0.pvrgiqn.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(db_link);
-const db = client.db("youtube");
-const users = db.collection("users");
-const usersdata = db.collection("usersdata");
-
 mongoose
   .connect(db_link)
-  .then(function (db) {
+  .then(async function () {
+    const client = new MongoClient(db_link);
+    await client.connect();
+    const db = client.db("youtube");
+    const users = db.collection("users");
+    const usersdata = db.collection("usersdata");
     console.log("db connected");
+
+    login.post("/login", (req, res) => {
+      try {
+        console.log("login req");
+        let email = req.body.email;
+        let password = req.body.password;
+        console.log(req.body);
+
+        const query = { email: `${email}` };
+        users.findOne(query).then((user) => {
+          try {
+            if (user) {
+              if (user.password == password) {
+                user.message = "login successfully";
+                res.cookie("login", true, {
+                  httpOnly: true,
+                  sameSite: "lax",
+                  maxAge: 24 * 60 * 60 * 1000,
+                });
+                res.cookie(`email`, `${email}`, {
+                  httpOnly: true,
+                  sameSite: "lax",
+                  maxAge: 24 * 60 * 60 * 1000,
+                });
+                res
+                  .cookie(`profilepicname`, `${user.profilepicname}`, {
+                    httpOnly: false,
+                    sameSite: "lax",
+                    maxAge: 24 * 60 * 60 * 1000,
+                  })
+                  // res.setHeader('Set-Cookie', [`login=true;HttpOnly`, `email=${email};HttpOnly`])
+                  .send(user);
+              } else {
+                user.message = "Wrong password";
+                res.send(user);
+              }
+            } else {
+              res.json({
+                message: "User not found",
+              });
+            }
+          } catch (error) {
+            console.log(error);
+          }
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    });
   })
   .catch(function (err) {
     console.log(err);
   });
-
-function postlogin(req, res) {
-  console.log(2);
-  let email = req.body.email;
-  let password = req.body.password;
-  console.log(req.body);
-  
-  const query = { email: `${email}` };
-  users.findOne(query).then((user) => {
-    if(user){
-      if (user.password == password) {
-        user.message = "login successfully";
-        res.cookie("login",true, { httpOnly: true,sameSite:"lax",maxAge:24* 60 * 60 * 1000  })
-        res.cookie(`email`,`${email}`, { httpOnly: true,sameSite:"lax",maxAge: 24 * 60 * 60 * 1000 })
-        res.cookie(`profilepicname`,`${user.profilepicname}`, { httpOnly: false,sameSite:"lax",maxAge: 24 * 60 * 60 * 1000 })
-        // res.setHeader('Set-Cookie', [`login=true;HttpOnly`, `email=${email};HttpOnly`])
-          .send(user);
-      } else {
-        user.message = "Wrong password";
-        res.send(user);
-      }
-    }
-    else{
-        res.json({
-          message : "User not found"
-        });
-    }
-  });
-}
 
 module.exports = login;
